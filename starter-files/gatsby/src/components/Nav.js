ad360bb84cf719ea226b4b8fcd2b892a54cf8315
Brett Saxon <brettcsaxon@gmail.com>
2020-10-04 19:37:12 -0500

    first commit

---
+++ starter-files/gatsby/src/components/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Nav = () => (
+  <nav>
+    <ul>
+      <li>
+        <a href="/">Home</a>
+      </li>
+      <li>
+        <a href="/beers">Beers</a>
+      </li>
+    </ul>
+  </nav>
+);
+export default Nav;
